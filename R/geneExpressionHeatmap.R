6e4e06f18b49a5b7c5dc4c9e84b55facf7f2bf0a
fflores97 <felipe_flores@dfci.harvard.edu>
2018-03-02 06:29:07 -0500

    Increased accepted graphical parameters

--- R/geneExpressionHeatmap.R
+++ R/geneExpressionHeatmap.R
@@ -8,11 +8,14 @@
 #' @param kMeans (optional) Number of k-means used to cluster rows
 #' @param fontSize Global font size
 #' @param title (optional) Heatmap Title
+#' @param annotationArgumnets (optional) List with further annotation parameters if needed
 #' @return hm, a heatmap object
 #' @author Felipe Flores
 #' @export
 
-geneExpressionHeatmap<- function(expressionMatrix,genesOfInterest,samples,annotationDataFrame,clusterColumns=T,clusterRows=T,fontSize,kMeans=1,title=character(0),...){
+geneExpressionHeatmap<- function(expressionMatrix,genesOfInterest,samples,annotationDataFrame,
+                                 clusterColumns=T,clusterRows=T,fontSize,kMeans=1,
+                                 title=character(0),annotationArguments=NULL,filename,...){
   if(missing(samples)){
     mat<-minimalSet[genesOfInterest,]
   } else{
@@ -30,18 +33,30 @@ geneExpressionHeatmap<- function(expressionMatrix,genesOfInterest,samples,annota
       names(colors[[i]])<-unique(type[[i]])
     }
     colors<-setNames(colors,names(type))
-    ha = ComplexHeatmap::HeatmapAnnotation(df = annotationDataFrame,col=colors)
+    ha.parameters <- list(df = annotationDataFrame,col=colors)
+    ha.parameters <- c(ha.parameters,as.list(annotationArguments))
+    ha <- do.call(ComplexHeatmap::HeatmapAnnotation,ha.parameters)
   } else {
     colors<-rainbow(length(unique(type)))
     names(colors)<-as.character(unique(type))
-    ha = HeatmapAnnotation(df = annotationDataFrame,col=list(type=colors))
+    colnames(annotationDataFrame)="type"
+    ha.parameters <- list(df = annotationDataFrame,col=list(type=colors))
+    ha.parameters <- c(ha.parameters,as.list(annotationArguments))
+    ha <- do.call(ComplexHeatmap::HeatmapAnnotation,ha.parameters)
   }
 
   ht_global_opt(heatmap_row_names_gp = gpar(fontface = "italic",fontsize=fontSize))
 
-  hm<-Heatmap(mat_scaled, name = "expression", col = colorRamp2(c(-2, 0, 2), c("blue", "white", "red")),
+  hm <-Heatmap(mat_scaled, name = "expression", col = colorRamp2(c(-2, 0, 2), c("blue", "white", "red")),
               top_annotation = ha, km=kMeans, top_annotation_height = unit(4, "mm"),
-              show_row_names = T, cluster_rows = clusterRows,show_column_names = FALSE,row_names_side = 'left',show_column_dend = T,cluster_columns = clusterColumns,column_title = title,...) +
-    Heatmap(base_mean, name = "base_mean", show_row_names = FALSE, width = unit(5, "mm"))
+              show_row_names = T, cluster_rows = clusterRows,show_column_names = FALSE,
+              row_names_side = 'left',show_column_dend = T,
+              cluster_columns = clusterColumns,column_title = title,...)
+  hm <- hm + Heatmap(base_mean, name = "base_mean", show_row_names = FALSE, width = unit(5, "mm"))
+  if(!missing(filename)) {
+    pdf(file = filename,width = 7,height=7)
+    print(hm)
+    dev.off()
+  }
   return(hm)
 }
